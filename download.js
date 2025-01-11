dd3e0c71c4442ba22710bd0eb51c51f33314a344
so <74056738+es3344520@users.noreply.github.com>
2025-01-11 12:38:18 +0800

    Update download.js

--- download.js
+++ download.js
@@ -1,19 +1,19 @@
 // download.js
 import path from 'path';
-import fs from 'fs';
+import { readFile } from 'fs/promises';
 
 export default async function handler(req, res) {
   try {
-    // 获取当前工作目录路径
-    const filePath = path.join(process.cwd(), 'public', 'profile.json');
+    // 获取当前工作目录路径，并指定profile.json的位置
+    const filePath = path.join(process.cwd(), 'profile.json');
     
     // 设置响应头以提示浏览器下载文件而不是显示它
     res.setHeader('Content-Disposition', 'attachment; filename=profile.json');
     res.setHeader('Content-Type', 'application/json');
 
-    // 读取文件并发送给客户端
-    const fileStream = fs.createReadStream(filePath);
-    fileStream.pipe(res);
+    // 同步读取文件并发送给客户端
+    const fileData = await readFile(filePath);
+    res.end(fileData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred while downloading the file.' });
